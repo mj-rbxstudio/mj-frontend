383de0a80c4ef1a5bb000b44b8f2e209ef642d0e
mj-rbxstudio <myu.melldy@gmail.com>
2026-04-15 07:25:08 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html>
       <body>{children}</body>
     </html>
   );
